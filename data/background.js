3683315e12b260633686c83914ee46bd5b89b9be
Ben Smith <binjimin@gmail.com>
2013-04-23 18:03:07 -0700

    Add browser action + icons

--- data/background.js
+++ data/background.js
@@ -1,7 +1,14 @@
 console.log('adding listener!');
 
+var enabled = false;
+
 chrome.webRequest.onHeadersReceived.addListener(
   function (info) {
+    if (!enabled) {
+      console.log('skipping '+info.url+' because disabled.');
+      return;
+    }
+
     console.log(info.url);
     var newHeaders = [];
     for (var i = 0; i < info.responseHeaders.length; ++i) {
@@ -23,6 +30,11 @@ chrome.webRequest.onHeadersReceived.addListener(
 
 chrome.webRequest.onHeadersReceived.addListener(
   function (info) {
+    if (!enabled) {
+      console.log('skipping '+info.url+' because disabled.');
+      return;
+    }
+
     console.log(info.url);
     var newHeaders = [];
     for (var i = 0; i < info.responseHeaders.length; ++i) {
@@ -41,6 +53,17 @@ chrome.webRequest.onHeadersReceived.addListener(
       responseHeaders: newHeaders
     };
   },
-  { urls: ['*://*/*.love'] },
+  { urls: ['*://*/*.love', '*://*/*.exe'] },
   ['blocking', 'responseHeaders']
+);
+
+var enabledIcon = {'path': {'19': 'nyu19_on.png', '38': 'nyu38_on.png'}};
+var disabledIcon = {'path': {'19': 'nyu19.png', '38': 'nyu38.png'}};
+
+chrome.browserAction.onClicked.addListener(
+  function (tab) {
+    enabled = !enabled;
+    console.log('browserAction clicked! enabled=' + enabled);
+    chrome.browserAction.setIcon(enabled ? enabledIcon : disabledIcon);
+  }
 );
